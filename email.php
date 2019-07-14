d51b66d47d276cce5aad2b56f8cfa57be8c9ffda
Ava <avalillie@leehome.ca>
2019-07-14 16:57:47 -0700

    Finished form, need to try it when uploaded from local source

---
+++ email.php
@@ -0,0 +1,16 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="aval8876@gmail.com";
+    $subject="Form to email message";
+    $name=$_POST["name"];
+    $email=$_POST["email"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $name\nEmail: $email\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $name <$email>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+?>
