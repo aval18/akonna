e9f4afee09363840d6438d129f53de30c87fbe6c
Ava <avalillie@leehome.ca>
2019-08-02 21:03:02 -0700

    Successful pages, need to hide the mobile links in the desktop view and fix the footer positioning

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,8 +1,24 @@
+// Mobile Nav
 function openNav() {
   document.getElementById("mobile").style.width = "100%";
 }
 
-/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
   document.getElementById("mobile").style.width = "0%";
 }
+
+// Pages
+
+function openPage(pageName) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, page, tab;
+  page = document.getElementsByClassName("page");
+  for (i = 0; i < page.length; i++) {
+    page[i].style.display = "none";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+}
+
+document.getElementById("defaultOpen").click();
