0b1e44af1b9d9940005bf01d8a37def337bc956f
Ava Lee <ava@Avas-MBP.localdomain>
2019-12-06 23:54:26 -0800

    trying on scroll header and fixing page openings

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -39,3 +39,14 @@ document.getElementById("defaultOpen").click();
 function openThanksMessage() {
   document.getElementById("thanks").style.display = "block";
 }
+
+//header
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.padding = "0";
+  } else {
+    document.getElementById("header").style.padding = "1rem";
+  }
+}
