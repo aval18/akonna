8ee558111a2941a4a8579d674fa35c36c20ef71b
Ava Lee <ava@MacBook-Pro-2.localdomain>
2020-05-17 17:38:47 -0700

    Added google analytics tag, removed page function from javascript

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -17,19 +17,3 @@ function openShop() {
 function closeShop() {
   document.getElementById("mobileShop").style.width = "0%";
 }
-
-// Pages
-
-function openPage(pageName) {
-  // Hide all elements with class="tabcontent" by default */
-  var i, page, tab;
-  page = document.getElementsByClassName("page");
-  for (i = 0; i < page.length; i++) {
-    page[i].style.display = "none";
-  }
-
-  // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
-}
-
-document.getElementById("defaultOpen").click();
